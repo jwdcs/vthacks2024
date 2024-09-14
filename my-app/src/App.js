26dfbf89b6b0c9b8beadc7f8a4776ce04b8ee550
jwdcs <jwdchutkowski@vt.edu>
2024-09-14 15:41:51 -0400

    Mockup for the game and the gamecard, getting flags in backend for countries.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import BrowserRouter, Routes, and Route
-import Battle from './Battle';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Game from './components/Game';
 import Header from './components/Header';
 import About from './pages/About';
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    // Fetch data from Flask API
-    fetch('http://localhost:5000/api/test')
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-      .catch(error => console.error('Error:', error));
-  }, []);
-
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/battle" element={<Battle />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/" element={<Game />} />
+        <Route path="/home" element={<Game />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<></>} />
       </Routes>
