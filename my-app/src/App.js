6738f6e8f918711c32cc1e8924007731395a74b0
mbrenn <mbrenn@vt.edu>
2024-09-13 23:36:38 -0400

    changed url

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   useEffect(() => {
     // Fetch data from Flask API
-    fetch('http://localhost:8000/api/test')
+    fetch('http://localhost:5000/api/test')
       .then(response => response.json())
       .then(data => setMessage(data.message))
       .catch(error => console.error('Error:', error));
